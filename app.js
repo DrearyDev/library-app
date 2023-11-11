0c30522bc5c7f136d477bf5677d41fffbfe33a9c
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-10 19:54:32 -0500

    Add functionality to add books to library

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ function Book(title, author, numOfPages, read) {
     this.read = read;
 };
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, numOfPages, read) {
     // do stuff..
+
+    myLibrary.push(new Book(title, author, numOfPages, read))
 };
+
+
+// test examples
+addBookToLibrary('harrypotter', 'jkr', 999, false);
+addBookToLibrary('fakebook', 'nobody', 0, true);
+
+console.log(myLibrary);
 
