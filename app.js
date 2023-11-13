adbebe1c5d24bb232861573d759128393c6230f0
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-12 21:27:04 -0500

    Add ability to add books from myLibrary array to page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const books = document.querySelector('.books');
+
 const myLibrary = [];
 
 function Book(title, author, numOfPages, read, img) {
@@ -20,12 +22,35 @@ function addBookToLibrary(title, author, numOfPages, read, img) {
 
 
 // test examples
-addBookToLibrary('harrypotter', 'jkr', 999, false);
-addBookToLibrary('fakebook', 'nobody', 0, true);
+addBookToLibrary('notharrypotter', 'jkr', 999, false, './example-covers/half-blood-prince.webp');
+addBookToLibrary('fakebook', 'nobody', 0, true, './example-covers/half-blood-prince.webp');
 
 function DisplayBooks(arr) {
     for (let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
+
+        let newBook = `
+        <div class="book">
+            <h1>${arr[i].title}</h1>
+            <img class="cover" src="${arr[i].img}">
+            <div class="information">
+                <div class="author">
+                    <p>Author:</p>
+                    <p>${arr[i].author}</p>
+                </div>
+                <div class="pages">
+                    <p># of pages:</p>
+                    <p>${arr[i].numOfPages}</p>
+                </div>
+                <div class="status">
+                    <p>Status:</p>
+                    <p>${arr[i].read}</p>
+                </div>
+            </div>
+            <img class="remove" src="./icons/exit.png">
+        </div>`;
+
+
+        books.innerHTML += newBook;
     };
 };
 
