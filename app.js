2d5b682a2ac718c0d82568bc596955ce1ece0046
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-11-13 18:28:56 -0500

    Add ability to add your own books to library

--- app.js
+++ app.js
@@ -5,82 +5,89 @@ const books = document.querySelector('.books');
 let remove = document.querySelectorAll('.remove');
 const formContainer = document.querySelector('.form-pop-up');
 const form = document.querySelector('.form-pop-up form');
+const formSubmit = document.querySelector('.form-pop-up form button');
 const addBookBtn = document.querySelector('.btn');
 
 const myLibrary = [];
 
-function Book(title, author, numOfPages, read, img) {
-    // the constructor...
-
+function Book(title, author, numOfPages, status, img) {
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
-    this.read = read;
+    this.status = status;
     this.img = img;
 };
 
-function addBookToLibrary(title, author, numOfPages, read, img) {
-    // do stuff..
+function addBookToLibrary(title, author, numOfPages, status, img) {
+    if (status === "off") {
+        status = 'not finished';
+    } else {
+        status = 'finished';
+    };
 
-    myLibrary.push(new Book(title, author, numOfPages, read, img))
+    let newBook = `
+    <div class="book">
+        <h1>${title}</h1>
+        <img class="cover" src="${img}">
+        <div class="information">
+            <div class="author">
+                <p>Author:</p>
+                <p>${author}</p>
+            </div>
+            <div class="pages">
+                <p># of pages:</p>
+                <p>${numOfPages}</p>
+            </div>
+            <div class="status">
+                <p>Status:</p>
+                <p>${status}</p>
+            </div>
+        </div>
+        <img class="remove" src="./icons/exit.png" alt="remove">
+    </div>`;
+
+    books.innerHTML += newBook;
+    myLibrary.push(new Book(title, author, numOfPages, status, img));
+
+    remove = document.querySelectorAll('.remove');
+
+    remove.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            if (e.target.offsetParent === form) {
+                formContainer.style.display = 'none';
+                body.style.overflow = 'auto';
+            } else {
+                e.target.offsetParent.style.display = 'none';
+            };
+        });
+    });
 };
-
 
 // test examples
-addBookToLibrary('notharrypotter', 'jkr', 999, false, './example-covers/half-blood-prince.webp');
-addBookToLibrary('fakebook', 'nobody', 0, true, './example-covers/half-blood-prince.webp');
-addBookToLibrary('realfakebook', 'everyone', 39847, false, './example-covers/half-blood-prince.webp');
-addBookToLibrary('catinhat', 'seuss', 389, true, './example-covers/half-blood-prince.webp');
-
-function DisplayBooks(arr) {
-    for (let i = 0; i < arr.length; i++){
-
-        let status;
-        if (arr[i].read === true) {
-            status = 'finished';
-        } else {
-            status = 'not finished'
-        };
-
-        let newBook = `
-        <div class="book">
-            <h1>${arr[i].title}</h1>
-            <img class="cover" src="${arr[i].img}">
-            <div class="information">
-                <div class="author">
-                    <p>Author:</p>
-                    <p>${arr[i].author}</p>
-                </div>
-                <div class="pages">
-                    <p># of pages:</p>
-                    <p>${arr[i].numOfPages}</p>
-                </div>
-                <div class="status">
-                    <p>Status:</p>
-                    <p>${status}</p>
-                </div>
-            </div>
-            <img class="remove" src="./icons/exit.png">
-        </div>`;
-
-        books.innerHTML += newBook;
-    };
-    remove = document.querySelectorAll('.remove');
-};
-DisplayBooks(myLibrary);
-
-remove.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        if (e.target.offsetParent === form) {
-            formContainer.style.display = 'none';
-            body.style.overflow = 'auto';
-        } else {
-            e.target.offsetParent.style.display = 'none';
-        };
-    });
-});
+addBookToLibrary('notharrypotter', 'jkr', 999, "off", './example-covers/half-blood-prince.webp');
+addBookToLibrary('fakebook', 'nobody', 0, "on", './example-covers/half-blood-prince.webp');
+addBookToLibrary('realfakebook', 'everyone', 39847, "off", './example-covers/half-blood-prince.webp');
+addBookToLibrary('catinhat', 'seuss', 389, "on", './example-covers/half-blood-prince.webp');
 
 addBookBtn.addEventListener('click', () => {
     formContainer.style.display = 'block';
     body.style.overflow = 'hidden';
+});
+
+console.log(remove);
+
+formSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(remove);
+
+    let title = e.target.offsetParent[0].value;
+    let author = e.target.offsetParent[1].value;
+    let numOfPages = e.target.offsetParent[2].value;
+    let status = e.target.offsetParent[3].value;
+    let img = URL.createObjectURL(e.target.offsetParent[4].files[0]);
+
+    formContainer.style.display = 'none';
+    body.style.overflow = 'auto';
+
+    addBookToLibrary(title, author, numOfPages, status, img);
 });
